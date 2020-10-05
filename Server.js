fe241ff930d3f83b15f545a39636488a80298d11
codersarathi <69192845+codersarathi@users.noreply.github.com>
2020-10-05 08:46:12 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,9 +1,19 @@
 const http = require('http');
+const readline = require('readline');
 
-const app = http.createServer((req,res) => {
-  res.end("<p>Hello</p>");
+const rl = readline.createInterface({
+  input : process.stdin,
+  output : process.stdout
+});
+
+rl.question("Enter you code: ",(ans) => {
+  const app = http.createServer((req,res) => {
+  res.end(ans);
 });
 
 app.listen(8080,() => {
   console.log("Server is started on 8080");
 })
+})
+
+
